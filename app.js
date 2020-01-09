f1468798d470b9c0e2829556025bf4914d3f6c4a
Johnathon Hutt <johnathonhutt@gmail.com>
2020-01-09 10:51:57 -0500

    Add build folder and route

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //jshint esversion:6
 
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", function(req, res){
-  res.send("It worked");
-});
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
 app.listen(port, function(){
   console.log(`Server is running on port ${port}`);
