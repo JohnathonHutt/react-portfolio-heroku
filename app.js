6dd07d09119a104783f3ddd8da3d071599f4ecbf
Johnathon Hutt <johnathonhutt@gmail.com>
2020-01-20 14:44:20 -0500

    Add CORS

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 //jshint esversion:6
 
+//dependencies
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
+
+//set port
 const port = process.env.PORT || 5000;
+
+//import futurama quotes
 const futurama = require("./futuramaQuotes");
 
-console.log(futurama.newQuote());
+//console.log(futurama.newQuote());
 
-app.get('/futurama-quote', function (req, res) {
-  let quote = futurama.newQuote();
-  res.send(quote);
+//use cors
+app.use(cors());
+
+//allow OPTIONS on all resources
+app.options('*', cors());
+
+//futurama api route
+app.get('/futurama', function (req, res) {
+  res.send(futurama.newQuote());
 });
 
+//react routes
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
