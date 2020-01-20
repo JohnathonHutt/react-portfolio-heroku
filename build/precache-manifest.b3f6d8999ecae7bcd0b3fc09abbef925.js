23183ba643b0d5714945310c9b850838b73fb113
Johnathon Hutt <johnathonhutt@gmail.com>
2020-01-20 13:19:33 -0500

    Update build for futurama-quotes get route

---
+++ build/precache-manifest.b3f6d8999ecae7bcd0b3fc09abbef925.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bbd71ebff66e71345243e95a704f1c",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "033e1ae3a6e8c51f0eb5",
+    "url": "/myapp/static/css/4.d2efdef1.chunk.css"
+  },
+  {
+    "revision": "b203ba264b71579ebc55",
+    "url": "/myapp/static/css/5.a193d83b.chunk.css"
+  },
+  {
+    "revision": "25a2c95f567dd5f7c834",
+    "url": "/myapp/static/css/6.87985c25.chunk.css"
+  },
+  {
+    "revision": "a40bd33fc3c971df88fb",
+    "url": "/myapp/static/css/7.3cd9d40a.chunk.css"
+  },
+  {
+    "revision": "b1a802f677fbe79b44d1",
+    "url": "/myapp/static/css/8.5b29740f.chunk.css"
+  },
+  {
+    "revision": "912acec57bfbccc1d8e1",
+    "url": "/myapp/static/css/9.a5300fb6.chunk.css"
+  },
+  {
+    "revision": "aa855c5d0397b79f49f2",
+    "url": "/myapp/static/css/main.74ad1fd6.chunk.css"
+  },
+  {
+    "revision": "9cdbcdc91b4abefccabc",
+    "url": "/myapp/static/js/2.fe5f0037.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/myapp/static/js/2.fe5f0037.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc6d05109b3ec4c8a4e2",
+    "url": "/myapp/static/js/3.34a492f1.chunk.js"
+  },
+  {
+    "revision": "033e1ae3a6e8c51f0eb5",
+    "url": "/myapp/static/js/4.cef453c1.chunk.js"
+  },
+  {
+    "revision": "b203ba264b71579ebc55",
+    "url": "/myapp/static/js/5.ae49d39f.chunk.js"
+  },
+  {
+    "revision": "25a2c95f567dd5f7c834",
+    "url": "/myapp/static/js/6.8d11abd9.chunk.js"
+  },
+  {
+    "revision": "a40bd33fc3c971df88fb",
+    "url": "/myapp/static/js/7.3609345a.chunk.js"
+  },
+  {
+    "revision": "b1a802f677fbe79b44d1",
+    "url": "/myapp/static/js/8.c7a05669.chunk.js"
+  },
+  {
+    "revision": "912acec57bfbccc1d8e1",
+    "url": "/myapp/static/js/9.5f996698.chunk.js"
+  },
+  {
+    "revision": "aa855c5d0397b79f49f2",
+    "url": "/myapp/static/js/main.c9828c7c.chunk.js"
+  },
+  {
+    "revision": "d3cd7f9deca112917f64",
+    "url": "/myapp/static/js/runtime-main.3f93cfb3.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25550488ce36b25ebaa2883a1002226e.js"
+  "/myapp/precache-manifest.b3f6d8999ecae7bcd0b3fc09abbef925.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
